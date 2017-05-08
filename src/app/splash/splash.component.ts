905730882d6f54da537bb015f11b8aef72fb5e79
TJ2001 <jung.m.tim@gmail.com>
2017-05-08 02:11:34 -0700

    add html templates

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-splash',
+  selector: 'splash',
   templateUrl: './splash.component.html',
   styleUrls: ['./splash.component.scss']
 })
