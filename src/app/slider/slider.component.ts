ff38424ff84faa91dbcc8d1bbd10acec7d748ff7
TJ2001 <jung.m.tim@gmail.com>
2017-05-15 00:12:45 -0700

    prepare to delete animationTwo

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -18,7 +18,9 @@ export class SliderComponent implements OnInit {
       this.height = "500px"
   };
 
-  function animation(trigger: string, otherCard: string, cardId: string){
+//possible rename of slide to pushUp
+
+  function animation(trigger: string, cardId: string, otherCard: string){
     $(trigger).click(function(){
       if($(otherCard).hasClass('appear')) {
         $(otherCard).addClass('fly');
@@ -35,7 +37,7 @@ export class SliderComponent implements OnInit {
     });
   }
 
-  function animationTwo(trigger: string, otherCard: string, cardId: string){
+  function animationTwo(trigger: string, cardId: string, otherCard: string){
     $(trigger).click(function(){
       if($(otherCard).hasClass('appear')) {
         $(otherCard).addClass('fly');
@@ -56,241 +58,51 @@ export class SliderComponent implements OnInit {
     let trigger = ".trigger" + this.index;
     let cardId = "#card" + this.index;
     let otherCard = ".card1";
-    animation(trigger, otherCard, cardId);
+    animation(trigger, cardId, otherCard);
   }
 
   Card.prototype.slide2 = function(){
     let trigger = ".trigger" + this.index;
     let cardId = "#card" + this.index;
     let otherCard = ".card2";
-    animationTwo(trigger, otherCard, cardId);
-  }
-
-  Card.prototype.slide2A = function(){
-    let trigger = ".trigger" + this.index;
-    let cardId = "#card" + this.index;
-    let otherCard = ".card2";
-    $(trigger).click(function(){
-      if($(otherCard).hasClass('appear')) {
-        $(otherCard).addClass('fly');
-        setTimeout(function(){
-          $(otherCard).removeClass('appear');
-          $(otherCard).removeClass('fly');
-          $(cardId).addClass('appear');
-        }, 500);
-        $(cardId).addClass('animated fadeInUp');
-        $(cardId).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-          $(cardId).removeClass('fadeInUp');
-        });
-      }
-    });
+    animation(trigger, cardId, otherCard);
   };
 
   Card.prototype.slide3 = function(){
     let trigger = ".trigger" + this.index;
     let cardId = "#card" + this.index;
     let otherCard = ".card3";
-    $(trigger).click(function(){
-      if($(otherCard).hasClass('appear')) {
-        $(otherCard).addClass('fly');
-        setTimeout(function(){
-          $(otherCard).removeClass('appear');
-          $(otherCard).removeClass('fly');
-          $(cardId).addClass('appear');
-        }, 500);
-        $(cardId).addClass('fadeInUp');
-        $(cardId).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-          $(cardId).removeClass('fadeInUp');
-        });
-      }
-    });
+    animation(trigger, cardId, otherCard);
   }
-
-  Card.prototype.slide3A = function(){
-    let trigger = ".trigger" + this.index;
-    let cardId = "#card" + this.index;
-    let otherCard = ".card3";
-    $(trigger).click(function(){
-      if($(otherCard).hasClass('appear')) {
-        $(otherCard).addClass('fly');
-        setTimeout(function(){
-          $(otherCard).removeClass('appear');
-          $(otherCard).removeClass('fly');
-          $(cardId).addClass('appear');
-        }, 500);
-        $(cardId).addClass('animated fadeInUp');
-        $(cardId).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-          $(cardId).removeClass('fadeInUp');
-        });
-      }
-    });
-  };
 
   Card.prototype.slide4 = function(){
     let trigger = ".trigger" + this.index;
     let cardId = "#card" + this.index;
     let otherCard = ".card4";
-
-    $(trigger).click(function(){
-      if($(otherCard).hasClass('appear')) {
-        $(otherCard).addClass('fly');
-        setTimeout(function(){
-          $(otherCard).removeClass('appear');
-          $(cardId).addClass('appear');
-        }, 500);
-        $(cardId).addClass('fadeInUp');
-        $(cardId).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-          $(cardId).removeClass('fadeInUp');
-          $(otherCard).removeClass('fly');
-        });
-      }
-    });
-  };
-
-  Card.prototype.slide4A = function(){
-    let trigger = ".trigger" + this.index;
-    let cardId = "#card" + this.index;
-    let otherCard = ".card4";
-    $(trigger).click(function(){
-      if($(otherCard).hasClass('appear')) {
-        $(otherCard).addClass('fly');
-        setTimeout(function(){
-          $(otherCard).removeClass('appear');
-          $(otherCard).removeClass('fly');
-          $(cardId).addClass('appear');
-        }, 500);
-        $(cardId).addClass('animated fadeInUp');
-        $(cardId).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-          $(cardId).removeClass('fadeInUp');
-        });
-      }
-    });
+    animation(trigger, cardId, otherCard);
   };
 
   Card.prototype.slide5 = function(){
     let trigger = ".trigger" + this.index;
     let cardId = "#card" + this.index;
     let otherCard = ".card5";
-
-    $(trigger).click(function(){
-      if($(otherCard).hasClass('appear')) {
-        $(otherCard).addClass('fly');
-        setTimeout(function(){
-          $(otherCard).removeClass('appear');
-          $(cardId).addClass('appear');
-        }, 500);
-        $(cardId).addClass('fadeInUp');
-        $(cardId).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-          $(cardId).removeClass('fadeInUp');
-          $(otherCard).removeClass('fly');
-        });
-      }
-    });
-  };
-
-  Card.prototype.slide5A = function(){
-    let trigger = ".trigger" + this.index;
-    let cardId = "#card" + this.index;
-    let otherCard = ".card5";
-    $(trigger).click(function(){
-      if($(otherCard).hasClass('appear')) {
-        $(otherCard).addClass('fly');
-        setTimeout(function(){
-          $(otherCard).removeClass('appear');
-          $(otherCard).removeClass('fly');
-          $(cardId).addClass('appear');
-        }, 500);
-        $(cardId).addClass('animated fadeInUp');
-        $(cardId).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-          $(cardId).removeClass('fadeInUp');
-        });
-      }
-    });
+    animation(trigger, cardId, otherCard);
   };
 
   Card.prototype.slide6 = function(){
     let trigger = ".trigger" + this.index;
     let cardId = "#card" + this.index;
     let otherCard = ".card6";
-
-    $(trigger).click(function(){
-      if($(otherCard).hasClass('appear')) {
-        $(otherCard).addClass('fly');
-        setTimeout(function(){
-          $(otherCard).removeClass('appear');
-          $(cardId).addClass('appear');
-        }, 500);
-        $(cardId).addClass('fadeInUp');
-        $(cardId).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-          $(cardId).removeClass('fadeInUp');
-          $(otherCard).removeClass('fly');
-        });
-      }
-    });
-  };
-
-  Card.prototype.slide6A = function(){
-    let trigger = ".trigger" + this.index;
-    let cardId = "#card" + this.index;
-    let otherCard = ".card6";
-    $(trigger).click(function(){
-      if($(otherCard).hasClass('appear')) {
-        $(otherCard).addClass('fly');
-        setTimeout(function(){
-          $(otherCard).removeClass('appear');
-          $(otherCard).removeClass('fly');
-          $(cardId).addClass('appear');
-        }, 500);
-        $(cardId).addClass('animated fadeInUp');
-        $(cardId).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-          $(cardId).removeClass('fadeInUp');
-        });
-      }
-    });
+    animation(trigger, cardId, otherCard);
   };
 
   Card.prototype.slide7 = function(){
     let trigger = ".trigger" + this.index;
     let cardId = "#card" + this.index;
     let otherCard = ".card7";
-
-    $(trigger).click(function(){
-      if($(otherCard).hasClass('appear')) {
-        $(otherCard).addClass('fly');
-        setTimeout(function(){
-          $(otherCard).removeClass('appear');
-          $(cardId).addClass('appear');
-        }, 500);
-        $(cardId).addClass('fadeInUp');
-        $(cardId).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-          $(cardId).removeClass('fadeInUp');
-          $(otherCard).removeClass('fly');
-        });
-      }
-    });
+    animation(trigger, cardId, otherCard);
   };
 
-  Card.prototype.slide7A = function(){
-    let trigger = ".trigger" + this.index;
-    let cardId = "#card" + this.index;
-    let otherCard = ".card7";
-    $(trigger).click(function(){
-      if($(otherCard).hasClass('appear')) {
-        $(otherCard).addClass('fly');
-        setTimeout(function(){
-          $(otherCard).removeClass('appear');
-          $(otherCard).removeClass('fly');
-          $(cardId).addClass('appear');
-        }, 500);
-        $(cardId).addClass('animated fadeInUp');
-        $(cardId).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-          $(cardId).removeClass('fadeInUp');
-        });
-      }
-    });
-  };
-
-  $( document ).ready(function() {
   let card1 = new Card(1, "blue");
   let card2 = new Card(2, "pink");
   let card3 = new Card(3, "orange");
@@ -310,52 +122,47 @@ export class SliderComponent implements OnInit {
 
   card1.initiate();
   card2.slide1();
-  card2.slide3A();
-  card2.slide4A();
+  card2.slide3();
+  card2.slide4();
   card2.slide5();
   card2.slide6();
-  card2.slide7A();
+  card2.slide7();
 
   card3.slide1();
   card3.slide2();
-  card3.slide4A();
+  card3.slide4();
   card3.slide5();
   card3.slide6();
   card3.slide7();
 
   card4.slide1();
   card4.slide2();
-  card4.slide3A();
-  card4.slide5A();
+  card4.slide3();
+  card4.slide5();
   card4.slide6();
   card4.slide7();
 
   card5.slide1();
   card5.slide2();
   card5.slide3();
-  card5.slide4A();
-  card5.slide6A();
-  card5.slide7A();
+  card5.slide4();
+  card5.slide6();
+  card5.slide7();
 
   card6.slide1();
-  card6.slide2A();
+  card6.slide2();
   card6.slide3();
-  card6.slide4A();
+  card6.slide4();
   card6.slide5();
   card6.slide7();
 
   card7.slide1();
   card7.slide2();
   card7.slide3();
-  card7.slide4A();
-  card7.slide5A();
-  card7.slide6A();
+  card7.slide4();
+  card7.slide5();
+  card7.slide6();
 
-    $('#card1').addClass('appear');
-
-
-  });
-
-
+  $('#card1').addClass('appear');
   }
 }
